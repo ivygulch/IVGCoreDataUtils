66f27fbfaada51dd6c9e5b14844e73af47b3fb08
Douglas Sjoquist <dwsjoquist@sunetos.com>
2015-12-14 09:41:10 -0500

    update pod info, add changes from trcshared

--- LibClasses/NSManagedObjectContext+IVGUtils.m
+++ LibClasses/NSManagedObjectContext+IVGUtils.m
@@ -12,69 +12,6 @@
 
 @implementation NSManagedObjectContext (IVGUtils)
 
-- (NSManagedObjectContext *) createChildContext;
-{
-    NSManagedObjectContext *result = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
-    result.parentContext = self;
-    return result;
-}
-
-- (BOOL) saveContextRecursively:(BOOL) recursively error:(NSError **) error;
-{
-    return [self saveContextRecursively:recursively modifiedPropertyName:@"modifiedTimestamp" error:error];
-}
-
-- (BOOL) saveContextRecursively:(BOOL) recursively modifiedPropertyName:(NSString *) modifiedPropertyName error:(NSError **) error;
-{
-    if (modifiedPropertyName) {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                 selector:@selector(objectContextWillSave:)
-                                                     name:NSManagedObjectContextWillSaveNotification
-                                                   object:self];
-    }
-    [self setAssociatedUserInfoObject:modifiedPropertyName forKey:@"modifiedPropertyName"];
-
-    BOOL result = YES;
-    NSManagedObjectContext *useContext = self;
-    while (useContext && result) {
-        if ([useContext hasChanges] && ![useContext save:error]) {
-            result = NO;
-            if (error) {
-                NSLog(@"save error: %@\n%@", [*error localizedDescription], [*error userInfo]);
-            }
-        }
-        useContext = recursively ? useContext.parentContext : nil;
-    }
-
-    if (modifiedPropertyName) {
-        [[NSNotificationCenter defaultCenter] removeObserver:self name:NSManagedObjectContextWillSaveNotification object:self];
-    }
-    [self setAssociatedUserInfoObject:nil forKey:@"modifiedPropertyName"];
-
-    return result;
-}
-
-- (void) objectContextWillSave:(NSNotification*) notification;
-{
-    NSString *modifiedPropertyName = [self associatedUserInfoObjectForKey:@"modifiedPropertyName"];
-    if (modifiedPropertyName == nil) {
-        return;
-    }
-
-    NSManagedObjectContext* context = [notification object];
-    NSSet* allModified = [context.insertedObjects setByAddingObjectsFromSet:context.updatedObjects];
-    NSMutableSet *modifiable = [NSMutableSet setWithCapacity:[allModified count]];
-    for (NSManagedObject *modified in allModified) {
-        if ([[modified.entity attributesByName] objectForKey:modifiedPropertyName] != nil) {
-            [modifiable addObject:modified];
-        }
-    }
-
-    NSDate *now = [NSDate date];
-    NSString *setterName = [NSString stringWithFormat:@"set%@%@:", [[modifiedPropertyName substringToIndex:1] uppercaseString], [modifiedPropertyName substringFromIndex:1]];
-    [modifiable makeObjectsPerformSelector:NSSelectorFromString(setterName) withObject:now];
-}
-
 - (id) insertNewEntityWithName:(NSString *)name {
     return [NSEntityDescription insertNewObjectForEntityForName:name inManagedObjectContext:self];
 }
@@ -117,43 +54,32 @@
     if ([results count] > 1) {
         NSLog(@"Expected one or zero records fetching from '%@' withPredicate: %@, found:\n%@", entityName, stringOrPredicate, results);
     }
-    return [results objectAtIndex:0 outOfRange:nil];
+    return [results firstObject];
 }
 
-#pragma mark - Class methods
-
-+ (NSURL *) writeableDatabaseUrl:(NSString *) databaseName error:(NSError **) errorPointer {
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    
-    NSString *docDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
-    NSString *writableDBPath = [docDir stringByAppendingPathComponent:databaseName];
-    
-    NSURL *storeUrl = [NSURL fileURLWithPath:writableDBPath];
-    
-    if([fileManager fileExistsAtPath:writableDBPath]) {
-        return storeUrl;
-    }
-    
-    // The writable database does not exist, so copy the default to the appropriate location.
-    NSString *defaultDBPath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:databaseName];
-    if ([fileManager copyItemAtPath:defaultDBPath toPath:writableDBPath error:errorPointer]) {
-        return storeUrl;
-    }
-    return nil;
+- (NSManagedObjectContext *) newChildContext;
+{
+    NSManagedObjectContext *result = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+    result.parentContext = self;
+    return result;
 }
 
-+ (NSManagedObjectContext *) newManagedObjectContextForDatastore:(NSURL *) storeUrl error:(NSError **) errorPointer {
-    NSManagedObjectModel *managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:nil];    
-    
-    NSPersistentStoreCoordinator *persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:managedObjectModel];
-    NSPersistentStore *persistentStore = [persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeUrl options:nil error:errorPointer];
-    if (!persistentStore) {
-        return nil;
+
+- (void) save;
+{
+    NSError *error = nil;
+    if (![self save:&error]) {
+        NSAssert(NO, @"Error saving context: %@", [error localizedDescription]);
     }
-    
-    NSManagedObjectContext *managedObjectContext = [[NSManagedObjectContext alloc] init];
-    [managedObjectContext setPersistentStoreCoordinator: persistentStoreCoordinator];
-    return managedObjectContext;
+}
+
+- (BOOL) isDescendantOf:(NSManagedObjectContext *) potentialAncestor;
+{
+    NSManagedObjectContext *checkContext = self.parentContext;
+    while ((checkContext != nil) && (checkContext != potentialAncestor)) {
+        checkContext = checkContext.parentContext;
+    }
+    return (potentialAncestor == checkContext);
 }
 
 @end
